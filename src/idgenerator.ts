e82a0a45e9509322d8efccbca91af119305e5a70
franius8 <skawinskifranciszek@gmail.com>
2022-10-29 10:09:47 +0200

    Enable local storage for ToDos

--- src/idgenerator.ts
+++ src/idgenerator.ts
@@ -1,11 +1,12 @@
 const idGenerator = (() => {
-  const iDsAry:number[] = []
+  const iDsAry:number[] = JSON.parse(localStorage.getItem("idsary") || "[]");
   const generateID = () => {
     while (true) {
       let randID:number = Math.floor(Math.random() * (10000));
       if (!iDsAry.includes(randID)) {
         iDsAry.push(randID);
-        return randID
+        localStorage.setItem('idsary', (JSON.stringify(iDsAry)));
+        return randID;
       }
     }
   }
--- src/todomanipulator.ts
+++ src/todomanipulator.ts
@@ -1,17 +1,28 @@
 import toDo from "./todo";
 import idGenerator from "./idgenerator";
+import dateFixer from "./datefixer";
 
 const toDoManipulator = (() => {
   interface ToDo {
     heading: string;
     text: string;
-    date: Date;
+    date: any;
     priority: string;
     iD: number;
     markAsDone: Function;
     getDoneStatus: Function;
   }
-  const toDoAry:ToDo[] = [];
+
+  let toDoAry:ToDo[] = [];
+
+  const loadToDoAry = () => {
+    toDoAry = JSON.parse(localStorage.getItem("todoary") || "[]");
+    toDoAry.forEach((todo: ToDo) => {
+      todo.date = dateFixer.fixDates(todo.date);
+    })
+  }
+
+  console.log(toDoAry);
   const createToDo = (heading: string, text: string, date: Date, priority: string) => {
     const iD = idGenerator.generateID();
     const newToDo:ToDo = toDo(heading, text, date, priority, iD);
@@ -37,9 +48,10 @@ const toDoManipulator = (() => {
   const deleteToDo = ( id:number ) => {
     const index:number = toDoAry.findIndex(x => x.iD === id);
     toDoAry.splice(index, 1);
+    localStorage.setItem('todoary', (JSON.stringify(toDoAry)));
   }
   const getToDoAry = () => toDoAry;
-  return { createToDo, modifyToDo, deleteToDo, getToDoAry }
+  return { createToDo, modifyToDo, deleteToDo, getToDoAry, loadToDoAry }
 })();
 
 export default toDoManipulator;
