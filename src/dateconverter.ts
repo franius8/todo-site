f39a5b909b8fba9c1f86220bfa804d28807fc9e0
franius8 <skawinskifranciszek@gmail.com>
2022-11-07 11:16:37 +0100

    Minor refactoring

--- src/dateconverter.ts
+++ src/dateconverter.ts
@@ -1,27 +1,22 @@
 const dateConverter = (() => {
   const convertToString = (date: Date) => {
-    let result = '';
   const day = date.getDate();
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
-  result = day + '.' + month + '.' + year;
-  return result;
+  return day + '.' + month + '.' + year;
   }
   const convertToInputFormat = (date: Date) => {
-    let result = '';
     const day = String(date.getDate()).padStart(2, '0');
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
-    result = year + '-' + month + '-' + day;
-    return result;
+    return year + '-' + month + '-' + day;
   }
 
   const getDayDifference = (date: Date) => {
     const toDoDate = date;
     const currentDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
     const timeDifference = toDoDate.getTime() - currentDate.getTime();
-    const dayDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
-    return dayDifference;
+    return Math.floor(timeDifference / (1000 * 3600 * 24));
   }
   return { convertToString, getDayDifference, convertToInputFormat };
 })();
