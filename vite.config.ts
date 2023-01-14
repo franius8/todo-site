9e377d5435ed07f71385176b7a15da3002cda5e8
franius8 <skawinskifranciszek@gmail.com>
2023-01-14 16:30:10 +0100

    Add config option for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todo-site/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ["firebase", "firebase/app", "firebase/auth", "firebase/firestore", "firebase/analytics"],
